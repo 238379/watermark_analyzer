fa3b89f200e80af5b65835fbdf5c5f7a4359ac4f
Dawid Antczak <238379@student.pwr.edu.pl>
2020-12-04 22:10:03 +0100

    Tests drafts for algorithms

---
+++ AlgorithmTest/AlgorithmTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using System.Drawing;
+
+namespace AlgorithmTest
+{
+	public class AlgorithmTests
+	{
+		protected void AssertBitmapsAreEqual(Bitmap bitmap1, Color[,] bitmap2)
+		{
+			bitmap1.Width.Should().Be(bitmap2.GetLength(1));
+			bitmap1.Height.Should().Be(bitmap2.GetLength(0));
+			for (int i = 0; i < bitmap1.Width; i++)
+			{
+				for (int j = 0; j < bitmap1.Height; j++)
+				{
+					bitmap1.GetPixel(i, j).Should().Be(bitmap2[i, j]);
+				}
+			}
+		}
+	}
+}
---
+++ AlgorithmTest/PixelAveragingTests.cs
@@ -0,0 +1,70 @@
+﻿using Algorithms;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace AlgorithmTest
+{
+	[TestFixture]
+	public class PixelAveragingTests : AlgorithmTests
+	{
+		private const int WIDTH = 2;
+		private const int HEIGHT = 2;
+
+		private Bitmap originalBitmap;
+		private Bitmap watermarkBitmap;
+		private double ratio;
+
+		private PixelAveragingParameters parameters;
+		private PixelAveraging algorithm;
+
+		[SetUp]
+		public void Setup()
+		{
+			originalBitmap = CreateOriginal();
+			watermarkBitmap = CreateWatermark();
+			ratio = 0.5;
+			parameters = new PixelAveragingParameters(originalBitmap, watermarkBitmap, null, ratio);
+
+			algorithm = new PixelAveraging(parameters);
+		}
+
+		[Test]
+		public void BasicAddingTest()
+		{
+			// Act
+			var results = algorithm.AddWatermark();
+
+			//var watermarked = results[0];
+
+			// Assert
+			//watermarked.Label.Should().Be("Watermarked");
+
+			//AssertBitmapsAreEqual(watermarked.Image, new Color[2, 2] {
+			//	{ Color.FromArgb(255, 0, 0), Color.FromArgb(1, 254, 0) },
+			//	{ Color.FromArgb(0, 0, 2), Color.FromArgb(0, 0, 3) }});
+
+			Assert.Fail("Remove if test is implemented.");
+		}
+
+		private Bitmap CreateOriginal()
+		{
+			var bmp = new Bitmap(WIDTH, HEIGHT);
+
+			bmp.SetPixel(0, 0, Color.FromArgb(255, 0, 0)); bmp.SetPixel(0, 1, Color.FromArgb(0, 254, 0));
+			bmp.SetPixel(1, 0, Color.FromArgb(0, 0, 3)); bmp.SetPixel(1, 1, Color.FromArgb(0, 0, 3));
+
+			return bmp;
+		}
+
+		private Bitmap CreateWatermark()
+		{
+			var bmp = new Bitmap(WIDTH, HEIGHT);
+
+			bmp.SetPixel(0, 0, Color.FromArgb(200, 0, 0)); bmp.SetPixel(0, 1, Color.FromArgb(128, 0, 127));
+			bmp.SetPixel(1, 0, Color.FromArgb(1, 1, 1)); bmp.SetPixel(1, 1, Color.FromArgb(0, 0, 255));
+
+			return bmp;
+		}
+	}
+}
